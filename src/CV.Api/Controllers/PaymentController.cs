edc8c4f78efff17c522e05f4eab8c363b49ece66
Sergii Samovyk <sergii.samovyk@arvosoftware.com>
2018-08-23 11:43:05 +0300

    Added empty Api project

---
+++ src/CV.Api/Controllers/PaymentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CV.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        // GET api/payment
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/payment/card
+        [HttpPost("card")]
+        public void Post([FromBody] string value)
+        {
+        }
+    }
+}
