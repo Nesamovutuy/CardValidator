4ebf84313f6700c2a22fd0ead71beca52324868f
Serhii Samovyk <serhii.samovyk@gmail.com>
2018-08-27 00:39:02 +0300

    Added React SPA client app with minimal markup

---
+++ src/CV.SPAClient/ClientApp/src/components/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export class Home extends Component {
+  displayName = Home.name
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Card Validation</h1>
+      </div>
+    );
+  }
+}
